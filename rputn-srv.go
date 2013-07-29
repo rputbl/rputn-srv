151c1e5df2ad274e6fd7973fb10cf3b8c94fd2c9
Charles Perkins <chuck@rputbl.com>
2013-07-29 11:14:37 -0700

    first commit

---
+++ rputn-srv.go
@@ -0,0 +1,352 @@
+package main
+
+
+import (
+    "runtime"
+    "fmt"
+    "bytes"
+    "os/exec"
+    "os"
+    "net/http"
+    "io/ioutil"
+    "log"
+    "time"
+//    "code.google.com/p/go.net/websocket"
+//    "kuracali.com/sessions"
+//    "kuracali.com/clients"
+)
+
+
+var hostname []byte
+var hb	HBstats
+
+func pwdfile(f string) string {
+	out := "File Read Error"
+	content, err := ioutil.ReadFile(f)
+	if err == nil {
+		out = string(content)
+	}
+	return out
+}
+
+
+
+
+
+func reply2request(req string) string{
+
+  return req
+}
+
+
+
+
+
+
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+    log.Printf("%s GET-HTML",hostname)
+    w.Header().Set("Content-Type", "text/html")
+    fmt.Fprintf(w, "%s", pwdfile("rputbl.html"))
+}
+
+func assertHandler(w http.ResponseWriter, r *http.Request) {
+    abc <- 1
+    log.Printf("%s ASSERT",hostname)
+    w.Header().Set("Content-Type", "application/json")
+      fmt.Fprintf(w, "%s", "{\"error\" : \"assert not understood\"}")
+}
+
+func queryHandler(w http.ResponseWriter, r *http.Request) {
+    qbc <- 1
+    log.Printf("%s QUERY",hostname)
+    w.Header().Set("Content-Type", "application/json")
+      fmt.Fprintf(w, "%s", "{\"error\" : \"query not understood\"}")
+}
+
+func ieCssHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/css")
+    fmt.Fprintf(w, "%s", pwdfile("ie.css"))
+}
+
+func printCssHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/css")
+    fmt.Fprintf(w, "%s", pwdfile("print.css"))
+}
+
+func screenCssHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/css")
+    fmt.Fprintf(w, "%s", pwdfile("screen.css"))
+}
+
+func buttonScreenCssHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/plain")
+    fmt.Fprintf(w, "%s", pwdfile("button-screen.css"))
+}
+
+func loginJsHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/plain")
+    fmt.Fprintf(w, "%s", pwdfile("login.js"))
+}
+
+func jqueryJsHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/plain")
+    fmt.Fprintf(w, "%s", pwdfile("jquery-1.4.4.min.js"))
+}
+
+func tickPngHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/plain")
+    fmt.Fprintf(w, "%s", pwdfile("tick.png"))
+}
+
+func authenticateHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    if 1==1{
+    log.Printf("AUTH-SUCCESS")
+      fmt.Fprintf(w, "%s", "{\"success\" : \"login is successful\", \"userid\" : \"33\"}")
+    }else{
+      fmt.Fprintf(w, "%s", "{{\"error\" : \"username or password is wrong\"}}")
+    }
+}
+
+
+func echoHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/plain")
+	fmt.Fprintf(w,"THIS IS FROM AJAX")
+//		ctx.Request.ParseForm()
+//		data := ctx.Request.FormValue("data")
+//		return reply2request(data)
+}
+
+
+type HBstats struct {
+	
+	Hostname [32]byte
+	Pid int
+	Assertions uint32
+	Hashcap uint32
+	Hashload uint32
+	Disksize uint32
+	Diskuse uint32
+	Repclan uint32
+	Shardspan uint32
+	Connections uint32
+	Qgsec [60]int
+	Agsec [60]int
+	Qbsec [60]int
+	Absec [60]int
+	Respms [60]int
+	Cpuload string
+	Memuse uint32
+	Netuse uint32
+}
+
+
+func Acc60( itm [60]int) int{
+
+	var acc int
+	acc = 0
+	for i:=0; i<60; i++{
+		acc = acc + itm[i]
+	}
+	return acc 
+}
+
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+
+    w.Header().Set("Content-Type", "application/json")
+
+
+
+	fmt.Fprintf(w,"{")
+	fmt.Fprintf(w,"\"hostname\" : \"%s\",",hostname)
+	fmt.Fprintf(w,"\"pid\" : \"%d\",",hb.Pid)
+	fmt.Fprintf(w,"\"assertions\" : \"%d\",",hb.Assertions)
+	fmt.Fprintf(w,"\"hashcap\" : \"%d\",",hb.Hashcap)
+	fmt.Fprintf(w,"\"hashload\" : \"%d\",",hb.Hashload)
+	fmt.Fprintf(w,"\"disksize\" : \"%d\",",hb.Disksize)
+	fmt.Fprintf(w,"\"diskuse\" : \"%d\",",hb.Diskuse)
+	fmt.Fprintf(w,"\"repclan\" : \"%d\",",hb.Repclan)
+	fmt.Fprintf(w,"\"shardspan\" : \"%d\",",hb.Shardspan)
+	fmt.Fprintf(w,"\"connections\" : \"%d\",",hb.Connections)
+	fmt.Fprintf(w,"\"qgsec\" : \"%d\",",Acc60(hb.Qgsec))
+	fmt.Fprintf(w,"\"agsec\" : \"%d\",",Acc60(hb.Agsec))
+	fmt.Fprintf(w,"\"qbsec\" : \"%d\",",Acc60(hb.Qbsec))
+	fmt.Fprintf(w,"\"absec\" : \"%d\",",Acc60(hb.Absec))
+	fmt.Fprintf(w,"\"respms\" : \"%d\",",Acc60(hb.Respms))
+	fmt.Fprintf(w,"\"cpuload\" : \"%s\",",hb.Cpuload)
+	fmt.Fprintf(w,"\"memuse\" : \"%d\",",hb.Memuse)
+	fmt.Fprintf(w,"\"netuse\" : \"%d\"",hb.Netuse)
+	fmt.Fprintf(w,"}")
+
+}
+
+func getHostname() {
+
+	cmd := exec.Command("hostname")
+	bs, err := cmd.Output()
+	if err != nil {  }
+	offset := bytes.IndexRune(bs,'.')
+	if offset < 1 { offset = len(bs) }
+	hostname = bs[:offset]
+	offset = bytes.IndexRune(hostname,'\n')
+	if offset < 1 { offset = len(hostname) }
+	hostname = hostname[:offset]
+
+
+}
+
+
+
+func getLoad()string {
+
+
+	switch runtime.GOOS {
+	case "windows": // ...
+		return "Windows"
+	case "linux": // ...
+		cmd := exec.Command("cat", "/proc/loadavg")
+		bs, err := cmd.Output()
+		if err != nil { return "top error" }
+		offset := bytes.IndexRune(bs,'\n')
+		if offset < 1 { offset = len(bs) }
+		return string(bs[:offset])
+	case "freebsd": // ...
+		return "freebsd"
+	case "darwin": // ...
+		cmd := exec.Command("sysctl", "-n", "vm.loadavg")
+		bs, err := cmd.Output()
+		if err != nil { return "top error" }
+		offset := bytes.IndexRune(bs,'\n')
+		if offset < 1 { offset = len(bs) }
+		return string(bs[:offset])
+	}
+	return "dunno!"
+
+
+
+}
+
+
+
+
+func listenForClientsSSL(){
+	log.Printf("Listening for clients on 8081/SSL")
+	err := http.ListenAndServeTLS(":8081", "cert.pem", "key.pem", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+func listenForClientsHTTP(){
+	log.Printf("Listening for clients on 8082/HTTP")
+	err := http.ListenAndServe(":8082", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+
+func housekeeping() {
+    for ; ; {
+	hb.Cpuload = getLoad()
+        time.Sleep(1 * time.Second)
+    }
+}
+
+var qgc chan int
+var agc chan int
+var qbc chan int
+var abc chan int
+var rmc chan int
+
+
+func qgrcv(){
+	var v int
+	for ;; {
+		v = <- qgc
+		hb.Qgsec[0]=hb.Qgsec[0]+v
+	}
+}
+func agrcv(){
+	var v int
+	for ;; {
+		v = <- agc
+		hb.Agsec[0]=hb.Agsec[0]+v
+	}
+}
+func qbrcv(){
+	var v int
+	for ;; {
+		v = <- qbc
+		hb.Qbsec[0]=hb.Qbsec[0]+v
+	}
+}
+func abrcv(){
+	var v int
+	for ;; {
+		v = <- abc
+		hb.Absec[0]=hb.Absec[0]+v
+	}
+}
+func rmrcv(){
+	var v int
+	for ;; {
+		v = <- rmc
+		hb.Respms[0]=hb.Respms[0]+v
+	}
+}
+
+
+func main() {
+	
+	getHostname()
+	go housekeeping()
+
+	hb.Pid = os.Getpid()
+	hb.Assertions = 0
+	hb.Hashcap = 0
+	hb.Hashload = 0
+	hb.Disksize = 0
+	hb.Diskuse = 0
+	hb.Repclan = 0
+	hb.Shardspan = 0
+	hb.Connections = 0
+
+	hb.Memuse = 0
+	hb.Netuse = 0
+
+	qgc = make(chan int)
+	agc = make(chan int)
+	qbc = make(chan int)
+	abc = make(chan int)
+	rmc = make(chan int)
+
+	go qgrcv()
+	go agrcv()
+	go qbrcv()
+	go abrcv()
+	go rmrcv()
+
+	http.HandleFunc("/", rootHandler)
+
+	http.HandleFunc("/a", assertHandler)
+
+	http.HandleFunc("/q", queryHandler)
+
+	http.HandleFunc("/ie.css", ieCssHandler)
+	http.HandleFunc("/print.css", printCssHandler)
+	http.HandleFunc("/screen.css", screenCssHandler)
+	http.HandleFunc("/buttons-screen.css", buttonScreenCssHandler)
+	http.HandleFunc("/login.js", loginJsHandler)
+	http.HandleFunc("/jquery-1.4.4.min.js", jqueryJsHandler)
+	http.HandleFunc("/tick.png", tickPngHandler)
+	http.HandleFunc("/status", statusHandler)
+	http.HandleFunc("/authenticate", authenticateHandler)
+
+	http.HandleFunc("/ejax", echoHandler)
+
+	go listenForClientsSSL()
+	listenForClientsHTTP()
+}
+
